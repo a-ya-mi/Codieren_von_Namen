aed8ef604b984ecc0c2d30f97ca0961ae63a5756
a-ya-mi <fattiavantia@gmail.com>
2017-05-11 13:26:44 +0200

    GUI

--- src/GuessTheName.java
+++ src/GuessTheName.java
@@ -2,15 +2,24 @@
  * Created by Aksana on 07.05.2017.
  */
 
+import sun.java2d.pipe.OutlineTextRenderer;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.security.SecureRandom;
 import java.io.*;
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class GuessTheName {
+
+public class GuessTheName extends JFrame{
 
     public static void main(String  []args){
+
+            JFrame jf = new OurFrame();
+
 
             try {
                 //random Name from Array (later from .txt)
