af2f9ac61de946d073b66c4c0c8d29ae485bdd62
a-ya-mi <fattiavantia@gmail.com>
2017-05-17 22:59:44 +0200

    more from GUI

--- src/OurFrame.java
+++ src/OurFrame.java
@@ -5,19 +5,22 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
 
 /**
  * Created by Aksana on 11.05.2017.
  */
 public class OurFrame extends JFrame {
     JLabel outputFromProgramm;
+    JTextField inputGuessFromUser;
     JButton buttonCheck;
     JButton buttonNewName;
     JButton buttonLevelUp;
-    JLabel inputGuessFromUser;
+
 
     public OurFrame() {
-        setVisible(true);
+
         setSize(500, 300);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,13 +31,20 @@ public class OurFrame extends JFrame {
         setLayout(null);
 
         outputFromProgramm = new JLabel("Here will be name");
-        outputFromProgramm.setBounds(120, 10, 500, 35);
+        outputFromProgramm.setBounds(20, 0, 200, 35);
         add(outputFromProgramm);
+
+        inputGuessFromUser = new JTextField();
+        inputGuessFromUser.setBounds(20, 50, 200, 35);
+        inputGuessFromUser.addCaretListener(new CursorInMove());
+        add(inputGuessFromUser);
+
 
         buttonCheck = new JButton("Klick and check your answer");
         buttonCheck.setBounds(250, 10, 200, 25);
         //xywh
         buttonCheck.addActionListener(new OurListener());
+        buttonCheck.setEnabled(false);
         add(buttonCheck);
 
         buttonNewName = new JButton("Guess next name");
@@ -48,15 +58,28 @@ public class OurFrame extends JFrame {
         //xywh
         add(buttonLevelUp);
 
+        setVisible(true);
     }
 
     private class OurListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent ae) {
-
-            outputFromProgramm.setText("Right or not Right");
+            String s = inputGuessFromUser.getText();
+            outputFromProgramm.setText(s);
         }
+    }
 
+ private class CursorInMove implements CaretListener {
+                @Override
+                public void caretUpdate(CaretEvent arg0){
+                    String s = inputGuessFromUser.getText();
+                    if (s.isEmpty()){
+                        buttonCheck.setEnabled(false);
+                    }
+                    else {
+                        buttonCheck.setEnabled(true);
+                    }
+                }
+            }
 
     }
-}
