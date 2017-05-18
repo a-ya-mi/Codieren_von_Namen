bced152a0c31216c22d0b81b841a335cc83e3093
a-ya-mi <fattiavantia@gmail.com>
2017-05-19 01:08:11 +0200

    zweite version gui

---
+++ src/quicklyGui.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+/**
+ * Created by Aksana on 19.05.2017.
+
+
+public class quicklyGui extends {
+     public quicklyGui() {
+        String inputGuess = JOptionPane.showInputDialog("Guess the name");
+     }
+}
