2f2ea5c132c2fb8a663cc09f997a78f5996fbec5
a-ya-mi <fattiavantia@gmail.com>
2017-06-01 16:27:40 +0200

    __ Start here! __

--- src/GuessTheName.java
+++ src/GuessTheName.java
@@ -2,16 +2,6 @@
  * Created by Aksana on 07.05.2017.
  */
 
-import sun.java2d.pipe.OutlineTextRenderer;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.security.SecureRandom;
-import java.io.*;
-
-import java.awt.*;
-import java.awt.event.*;
-import java.util.ArrayList;
 import javax.swing.*;
 
 
@@ -19,7 +9,6 @@ public class GuessTheName extends JFrame{
 
 
     public static void main(String  []args){
-
 
             JFrame jf = new OurFrame();
 
