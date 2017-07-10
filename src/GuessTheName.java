3367a3b079b10c8630ea61df9e94bcb02aba62f2
a-ya-mi <fattiavantia@gmail.com>
2017-07-10 18:58:42 +0200

    Start here

--- src/GuessTheName.java
+++ src/GuessTheName.java
@@ -8,7 +8,7 @@ import javax.swing.*;
 public class GuessTheName extends JFrame{
 
     public static void main(String  []args){
-
+        //TO DO gui anders
         JFrame jf = new OurFrame();
 
         }
